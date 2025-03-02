cda45588fc5bcf9cefa20ad9bc4be56c98dafe94
Jai.Gupta <jai.gupta@admins-MacBook-Pro.local>
2025-03-02 14:34:33 +0530

    MetaData added

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import { SanityLive } from "@/sanity/lib/live";
+import { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 
 export default function UsertLayout({
@@ -19,3 +20,31 @@ export default function UsertLayout({
       </SessionProvider>
   );
 }
+
+export const metadata: Metadata = {
+  title: "The Prototype Studio - Learn, Innovate, Succeed",
+  description: "Empowering learners with hands-on coaching in tech, design, and innovation.",
+  keywords: "coaching, tech coaching, design learning, innovation, The Prototype Studio, skill development",
+  authors: [{name: "The Prototype Studio"}],
+  openGraph: {
+    title: "The Prototype Studio - Learn, Innovate, Succeed",
+    description: "Join The Prototype Studio to master technology, design, and innovation with expert coaching.",
+    url: "https://theprototypestudio.com",
+    siteName: "The Prototype Studio",
+    images: [
+      {
+        url: "/logo.webp",
+        width: 1200,
+        height: 630,
+        alt: "The Prototype Studio Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "The Prototype Studio - Learn, Innovate, Succeed",
+    description: "Master cutting-edge skills with expert coaching at The Prototype Studio.",
+    images: ["https://theprototypestudio.com/twitter-image.jpg"],
+  },
+};

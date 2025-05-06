06f3f7b65eca5198ea953f4a5abfd6f75789757e
Jai.Gupta <jai.gupta@admins-MacBook-Pro.local>
2025-05-06 15:44:01 +0530

    6th May google adsense preview fix

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -20,11 +20,11 @@ const HomePage = async () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-16">
-          {courses.map((course) => (
+          {courses?.map((course: any) => (
             <CourseCard
-              key={course._id}
+              key={course?._id}
               course={course}
-              href={`/courses/${course.slug}`}
+              href={`/courses/${course?.slug}`}
             />
           ))}
         </div>

6acfa32b5eb9c59784c6ba5222f02b401a67dd21
Jai.Gupta <jai.gupta@admins-MacBook-Pro.local>
2025-05-06 16:13:01 +0530

    6th May 2nd attempt

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -20,7 +20,7 @@ const HomePage = async () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-16">
-          {courses?.map((course: any) => (
+          {courses && courses?.map((course: any) => (
             <CourseCard
               key={course?._id}
               course={course}

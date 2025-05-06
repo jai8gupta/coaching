49c0b3fac80b0debf263d6cb1f75cb84bc1670f3
Jai.Gupta <jai.gupta@admins-MacBook-Pro.local>
2025-05-06 15:50:51 +0530

    16th April remove the home page contents temporarily

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -19,7 +19,7 @@ const HomePage = async () => {
           <div className="h-px flex-1 bg-gradient-to-r from-border/0 via-border to-border/0" />
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-16">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-16">
           {courses?.map((course: any) => (
             <CourseCard
               key={course?._id}
@@ -27,7 +27,7 @@ const HomePage = async () => {
               href={`/courses/${course?.slug}`}
             />
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   )
